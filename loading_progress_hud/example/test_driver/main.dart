41156ef462efd9760395ec45d11ea8e9c01f8672
lilei <445765368@qq.com>
2019-05-21 18:14:06 +0800

    配置

--- loading_progress_hud/example/test_driver/main.dart
+++ loading_progress_hud/example/test_driver/main.dart
@@ -1,7 +1,7 @@
-import 'package:example/main.dart' as app;
-import 'package:flutter_driver/driver_extension.dart';
+// import 'package:example/main.dart' as app;
+// import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
-  enableFlutterDriverExtension();
-  app.main();
+  // enableFlutterDriverExtension();
+  // app.main();
 }
